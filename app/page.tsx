b2606ec9415c20b16efbb002334f42deecd6713f
nicochnz <chiche.nicolas@gmail.com>
2026-02-05 08:02:16 +0100

    change: body bg

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "sonner";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-secondary">
       <Navbar />
       <Hero />
       <MenuSection />
